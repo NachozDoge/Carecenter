d9a7f3908e05ffc290294509f18803c994e8e589
AndersonMagalhaes27 <andersonmagalhaes027@gmail.com>
2020-12-02 01:10:43 -0300

    tela de login/cadastro/cadastro-perfil/home

--- src/app/cadastrar/cadastrar.page.ts
+++ src/app/cadastrar/cadastrar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private menuCtrl : MenuController,
+  ) {
+    this.ionViewWillEnter();
+   }
 
   ngOnInit() {
   }
 
+
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
 }
