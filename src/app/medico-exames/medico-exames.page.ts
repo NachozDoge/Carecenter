fad907756b7363e25d5ccc4bfc01b1470765b889
Tukaku <iphone11gtx@gmail.com>
2021-02-04 20:08:28 -0300

    asdasd
    adasd

--- src/app/medico-exames/medico-exames.page.ts
+++ src/app/medico-exames/medico-exames.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Medico } from '../model/medico';
+import { MedicoService } from '../services/medico.service';
 
 @Component({
   selector: 'app-medico-exames',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoExamesPage implements OnInit {
 
-  constructor() { }
+  @ViewChild("nome") nome; 
+
+  lista : Medico[] = [];
+  espc: any;
+
+
+  constructor(private medicoServ : MedicoService,
+    private navCtrl : NavController) { }
 
   ngOnInit() {
+    this.medicoServ.listaDemedico().subscribe(response=>{
+      // O servidor respondeu
+      
+      this.lista = response;
+     
+
+      
+    },err=>{
+      // erro
+    })
   }
+
+  visualizar(id){
+    this.navCtrl.navigateForward(['/exame-visualizar',id])
+  }
+
+  agendar(id){
+    this.navCtrl.navigateForward(['/agendar-exame',id])
+  }
+
+  pesquisar(){
+    console.log("Busca por: "+this.nome.value)
+    this.medicoServ.buscaPorEspecialidade(this.nome.value).subscribe(response=>{
+      this.lista = [];
+      this.lista = response;
+    });
+  }
+  
 
 }
